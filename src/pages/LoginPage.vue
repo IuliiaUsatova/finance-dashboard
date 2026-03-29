6e07aee59048d519b72c11a2df2cd8f24cecc4a0
ivanovDmirty <ivanov.dmitrii@magicslide.kz>
2026-03-29 11:25:20 -0700

    feat: connect auth store to LoginPage with redirect

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,21 +1,35 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-const email = ref('')
-const password = ref('')
+import { ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useRouter } from "vue-router";
+import { useAuthStore } from "../stores/auth";
+
+const authStore = useAuthStore();
+const { user, isAuthenticated } = storeToRefs(authStore);
+const { login, logout } = authStore;
+const router = useRouter();
+const email = ref("");
+const password = ref("");
 
 function handleLogin() {
-    console.log(email.value, password.value);
+    const userData = {
+        id: 1,
+        name: "Юля",
+        email: email.value,
+    };
+    login(userData);
+    router.push('/')
 }
 </script>
 <template>
     <div>
         <div class="email">
             <p>Email</p>
-            <input type="email" v-model="email">
+            <input type="email" v-model="email" />
         </div>
         <div class="password">
             <p>Пароль</p>
-            <input type="password" v-model="password">
+            <input type="password" v-model="password" />
         </div>
         <button @click="handleLogin">Войти</button>
     </div>
