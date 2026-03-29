ef3ee6020677198be285c5a74b93f7f4edf92a4d
ivanovDmirty <ivanov.dmitrii@magicslide.kz>
2026-03-29 11:03:17 -0700

    feat: add LoginPage with email/password form

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const email = ref('')
+const password = ref('')
+
+function handleLogin() {
+    console.log(email.value, password.value);
+}
+</script>
 <template>
-  <h1>Login</h1>
+    <div>
+        <div class="email">
+            <p>Email</p>
+            <input type="email" v-model="email">
+        </div>
+        <div class="password">
+            <p>Пароль</p>
+            <input type="password" v-model="password">
+        </div>
+        <button @click="handleLogin">Войти</button>
+    </div>
 </template>
