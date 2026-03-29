78a0c8ffce594d7985d849d9853708e0aa7a42b0
ivanovDmirty <ivanov.dmitrii@magicslide.kz>
2026-03-29 11:53:16 -0700

    feat: add navigation guard to protect routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../pages/LoginPage.vue'
 import DashboardPage from '../pages/DashboardPage.vue'
 import TransactionsPage from '../pages/TransactionsPage.vue'
+import { useAuthStore } from "../stores/auth"
+
 
 const routes = [
     {
@@ -21,4 +23,11 @@ const routes = [
 export const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from) => {
+    const authStore = useAuthStore()
+    if (!authStore.isAuthenticated && to.path !== '/login') {
+        return '/login'
+    }
 })
