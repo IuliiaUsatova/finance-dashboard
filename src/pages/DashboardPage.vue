dab02aced00425d6bc0de867db47af9fae7f9a5e
ivanovDmirty <ivanov.dmitrii@magicslide.kz>
2026-04-04 22:19:47 -0700

    feat: add transactions store and balance to DashboardPage

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
 import { useAuthStore } from "../stores/auth";
+import { useTransactionsStore } from "../stores/transactions"
 import { useRouter } from "vue-router";
+// import { onMounted } from "vue";
+// import { getTransactions } from '../api/transactions'
 
 
 const authStore = useAuthStore();
+const transactionsStore = useTransactionsStore()
 const { user } = storeToRefs(authStore);
 const { logout } = authStore;
+const { balance } = storeToRefs(transactionsStore)
 const router = useRouter()
+
+// onMounted(async () => {
+//   const data = await getTransactions()
+//   transactionsStore.transactions = data
+// })
 
 function handleLogout() {
   logout()
@@ -18,7 +28,7 @@ function handleLogout() {
 <template>
   <div>
     <h1>Привет {{ user?.name }}</h1>
-    <p>Баланс: 0</p>
+    <p>Баланс: {{ balance }}</p>
     <button @click="handleLogout">Выйти из кабинета</button>
   </div>
 </template>
